43e47329e7899790088da7ac53204805f6427052
ChorizoMagico <juanestebanarias.2006@gmail.com>
2025-09-22 21:57:13 -0500

    1.2.1 y 1.2.2

--- src/main/scala/Comparador/package.scala
+++ src/main/scala/Comparador/package.scala
@@ -41,5 +41,49 @@ package object Comparador {
 
   }
 
+  def menoresQue_noMenoresQue[T](l: List[T], v: T, comp: Comparador[T]): (List[T], List[T], Int) =
+  {
+
+    def ordenar(l1: List[T], l2: List[T], l3: List[T], v: T, counter: Int, comp: Comparador[T]): (List[T], List[T], Int) = {
+
+      if(l3.isEmpty){
+        (l1, l2, counter)
+      }else{
+        if(comp(l3.head, v)) ordenar(l1, (l3.head)::l2, l3.tail, v, counter+1, comp)
+        else ordenar((l3.head)::l1, l2, l3.tail, v, counter+1, comp)
+      }
+
+    }
+
+    ordenar(List(), List(), l.tail, v, 0, comp)
+
+  }
+
+  def quickSort[T](comp:Comparador[T]): AlgoritmoOrd[T] = {
+
+    def AlgoritmoOrd (l:List[T]):  (List[T], Int) = {
+
+      def ordenar(l: List[T], counter: Int, comp: Comparador[T]): (List[T], Int) = {
+
+        if(l.isEmpty) (l, counter)
+        else{
+
+          val parList = menoresQue_noMenoresQue(l, l.head, comp)
+
+          val subParList = ordenar(parList._1, counter + parList._3, comp)
+          val overParList = ordenar(parList._2, subParList._2, comp)
+
+          (subParList._1 ++ List(l.head) ++ overParList._1, overParList._2)
+
+        }
+      }
+
+      ordenar(l, 0, comp)
+    }
+
+    AlgoritmoOrd
+  }
+
+
 
 }
